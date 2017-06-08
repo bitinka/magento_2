89b9dbfc4eec63431ce6e161d43ca477c8141fca
Leonardo De Sousa <ldesousa@mail.bitinka.com>
2017-06-08 10:30:53 -0400

    comentario

---
+++ Pay/Payment/etc/Controller/Redirect/SendMail.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Pay\Payment\Controller\Redirect;
+
+use Magento\Sales\Model\Order;
+use Magento\Sales\Model\Config\Source\Order\Status;
+
+class SendMail extends \Magento\Framework\App\Action\Action
+{
+
+
+protected $_customerSession;
+protected $order;
+protected $om;
+
+public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Sales\Model\OrderFactory $orderFactory,
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Sales\Model\Order $order
+      ) {
+        parent::__construct($context);
+        $this->_customerSession = $customerSession;
+        $this->_order = $order;
+        $this->om =   \Magento\Framework\App\ObjectManager::getInstance();
+      }
+
+public function execute()
+    {
+      $ordData = $this->_order->load($_POST['nOrder']);
+
+        if($_POST['code'] == 0){
+          $ordData->setState('complete', true);
+          $ordData->setStatus('complete', true);
+        }else{
+          $ordData->setState('pending_payment', true);
+          $ordData->setStatus('pending_payment', true);
+        }
+
+         try {
+            $ordData->save();
+        } catch (\Magento\Framework\Exception\LocalizedException $e) {
+            $this->messageManager->addError($e->getMessage());
+        }
+
+
+        $order = $this->_objectManager->create('Magento\Sales\Model\Order')->load($_POST['nOrder']);
+        $email = $order->getCustomerEmail();
+        if ($order) {
+            try {
+                $this->_objectManager->create('\Magento\Sales\Model\OrderNotifier')
+                    ->notify($order);
+                $this->messageManager->addSuccess(__('Your order are procesad.'));
+            } catch (\Magento\Framework\Exception\LocalizedException $e) {
+                $this->messageManager->addError($e->getMessage());
+            } catch (\Exception $e) {
+                $this->messageManager->addError(__('We can\'t send the email order right now.'));
+                $this->_objectManager->create('Magento\Sales\Model\OrderNotifier')->critical($e);
+            }
+        }
+        $this->_redirect('customer/account/login');
+    }
+
+}
---
+++ Pay/Payment/etc/Controller/Redirect/SendOrder.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Pay\Payment\Controller\Redirect;
+
+class SendOrder extends \Magento\Framework\App\Action\Action {
+
+  protected $_checkoutSession;
+    protected $_customerSession;
+    protected $_orderFactory;
+    protected $om;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Sales\Model\OrderFactory $orderFactory
+    )
+    {
+        $this->_checkoutSession = $checkoutSession;
+        $this->_orderFactory = $orderFactory;
+        $this->om =   \Magento\Framework\App\ObjectManager::getInstance();
+        $this->_customerSession = $customerSession;
+        return parent::__construct($context);
+    }
+
+
+
+    public function execute() {
+            $nOrder = $this->_getOrder()->getId();
+            $order = $this->om->create('Magento\Sales\Model\Order')->load($nOrder);
+            $orderItems = $order->getAllItems();
+
+            $total = 0;
+
+            //detail order
+            foreach ($orderItems as $item)
+            {
+                $total = $total + $item['base_row_total_incl_tax'];
+            }
+            $this->_sendForm($nOrder,$total);
+}
+
+    protected function _getOrder()
+    {
+        $orderIncrementId = $this->_checkoutSession->getLastRealOrderId();
+        $order = $this->_orderFactory->create()->loadByIncrementId($orderIncrementId);
+        return $order;
+    }
+
+    protected function _sendForm($nOrder,$total)
+    {
+        echo '<form action="http://127.0.0.1/inkapay/ecommerce/coins" method="post" name="formulario1">
+            <div class="form-group">
+              <input type="hidden" name="nOrder" value="'.$nOrder.'">
+            </div>
+            <div class="form-group">
+              <input type="hidden" name="create" value="'.date("d/m/Y").'">
+            </div>
+            <div class="form-group">
+              <input type="hidden" name="total" value="'.$total.'">
+            </div>
+            <div class="form-group">
+              <input type="hidden" name="token" value="'.$this->_customerSession->getAccess_token().'">
+            </div>
+        </form>';
+        echo '<script type="text/javascript">
+          document.formulario1.submit();
+        </script>';
+    }
+}
